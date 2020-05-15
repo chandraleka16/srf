93f2abbf36f3ad131bb6a83ee3ca1fbef620edd5
chandraleka16 <42545930+chandraleka16@users.noreply.github.com>
2020-05-15 09:57:09 +0530

    changed db name

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = mysqli_connect("localhost", "root", "", "db_user");
+	$conn = mysqli_connect("localhost", "root", "", "lamp_task");
 	
 	if(!$conn){
 		die("Error: Failed to connect to database!");
