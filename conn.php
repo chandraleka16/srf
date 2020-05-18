e182cc1495aac455ad7e740e6484a61fbfca0fe9
chandraleka16 <42545930+chandraleka16@users.noreply.github.com>
2020-05-18 14:47:56 +0530

    changed username

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = mysqli_connect("localhost", "mychan_username", "password", "lamp_task");
+	$conn = mysqli_connect("localhost", "mychan_user", "password", "lamp_task");
 	
 	if(!$conn){
 		die("Error: Failed to connect to database!");
